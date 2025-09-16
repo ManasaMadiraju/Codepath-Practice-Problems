744074f7ce3d0c13f745e88f1c193f45c69893b3
ManasaMadiraju <manasamadiraju343@gmail.com>
2025-09-16 15:41:03 -0700

    Standard Problem Set V1

---
+++ Unit-1/P1.py
@@ -0,0 +1,5 @@
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+if __name__ == '__main__':
+    welcome()
---
+++ Unit-1/P2.py
@@ -0,0 +1,6 @@
+def greeting(name):
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin.")
+
+if __name__ == '__main__':
+    greeting("Michael")
+    greeting("Winnie the Pooh")
