2022647dd9ec6b0794f84db3eabb6311fe79a309
ManasaMadiraju <manasamadiraju343@gmail.com>
2025-11-04 23:07:38 -0600

    Standard Problem Set Version 1

--- Unit-1/Session 1/Standard Problem Set Version 1/P10.py
+++ Unit-1/Session 1/Standard Problem Set Version 1/P10.py
@@ -1,3 +1,5 @@
+'''Piglet's has collected a big pile of his favorite food, haycorns, and wants to split them evenly amongst his friends. Write a function split_haycorns() to help Piglet determine the number of ways he can split his haycorns into even groups. split_haycorns() accepts a positive integer quantity as a parameter and returns a list of all divisors of quantity.'''
+
 def split_haycorns(quantity):
     divisors = []
     for i in range(1, quantity+1):
--- Unit-1/Session 1/Standard Problem Set Version 1/P11.py
+++ Unit-1/Session 1/Standard Problem Set Version 1/P11.py
@@ -1,3 +1,5 @@
+'''Signs in the Hundred Acre Wood have been losing letters as Tigger bounces around stealing any letters he needs to spell out his name. Write a function tiggerfy() that accepts a string s, and returns a new string with the letters t, i, g, e, and r removed from it.'''
+
 def tiggerfy(s):
     stolen = 'tiger'
     result = ''
