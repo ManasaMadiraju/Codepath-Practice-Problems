92083804cf692672de693117da0ddfc8969d035f
ManasaMadiraju <manasamadiraju343@gmail.com>
2025-11-10 21:06:53 -0800

    Advanced Problem Set Version 1

--- Unit-1/Session 1/Advanced Problem Set Version 1/P1.py
+++ Unit-1/Session 1/Advanced Problem Set Version 1/P1.py
@@ -1,11 +1,12 @@
+'''Write a function linear_search() to help Winnie the Pooh locate his lost items. The function accepts a list items and a target value as parameters. The function should return the first index of target in items, and -1 if target is not in items. Do not use any built-in functions.'''
+
 def linear_search(items, target):
-    for key,value in enumerate(items):
+    for key, value in enumerate(items):
         if value == target:
-            return key
+           return key
     return -1
+        
     
-
-
 if __name__ == '__main__':
     items = ['haycorn', 'haycorn', 'haycorn', 'hunny', 'haycorn']
     target = 'hunny'
