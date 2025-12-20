f9c36ecd44c310d493e12960c943c7851345d915
ManasaMadiraju <manasamadiraju343@gmail.com>
2025-12-19 23:07:06 -0800

    Solved

--- Week-1/Session 2/Standard Problem Set Version 2/P10.py
+++ Week-1/Session 2/Standard Problem Set Version 2/P10.py
@@ -9,24 +9,42 @@ Write a function expose_superman() that accepts a 2D array trust where trust[i] 
 
 Return the label of Superman if he is hiding amongst the population and can be identified, or return -1 otherwise.'''
 
+ #Approach 1
 def expose_superman(trust, n):
-    population = set()
-    hashmap = {}
-    possible_supermans = []
-    for i in trust: #[1,3]
-        hashmap[i[0]] = i[1]
-        population.add(i[0])
-        population.add(i[1])
-    for value in population:
-        if value not in hashmap.keys():
-            possible_supermans.append(value)
+    if n == 1:
+        return 1
 
-    trusted_people = list(hashmap.values())
-    for superman in possible_supermans:
-        if trusted_people.count(superman) == n - 1:
-            return superman   
+    in_degree = [0] * (n + 1)
+    out_degree = [0] * (n + 1)
+
+    for a, b in trust:
+        out_degree[a] += 1
+        in_degree[b] += 1
+
+    for person in range(1, n + 1):
+        if out_degree[person] == 0 and in_degree[person] == n - 1:
+            return person
+
     return -1
 
+#Approach 2
+#def expose_superman(trust, n):
+    # population = set()
+    # hashmap = {}
+    # possible_supermans = []
+    # for i in trust: #[1,3]
+    #     hashmap[i[0]] = i[1]
+    #     population.add(i[0])
+    #     population.add(i[1])
+    # for value in population:
+    #     if value not in hashmap.keys():
+    #         possible_supermans.append(value)
+
+    # trusted_people = list(hashmap.values())
+    # for superman in possible_supermans:
+    #     if trusted_people.count(superman) == n - 1:
+    #         return superman   
+    # return -1
 
 if __name__ == '__main__':
     n = 2
